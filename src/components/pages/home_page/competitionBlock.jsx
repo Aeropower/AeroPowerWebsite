cf2f7d8b36d2dd9245d4c5b2103da8671e15e2e5
Nelson Nin <nelson.nin@upr.edu>
2025-03-04 20:26:14 -0400

    Added videos
    Added Youtube videos
    Fix spacing

--- src/components/pages/home_page/competitionBlock.jsx
+++ src/components/pages/home_page/competitionBlock.jsx
@@ -4,12 +4,32 @@ const competitionBlock = () => {
     return (
         <div className="competition-block">
             <div className="competitions-grid">
-                <div className="child1"><p className="competition-title">Collegiate Wind Competition (CWC)</p></div>
+                <p className="competition-title">Collegiate Wind Competition (CWC)</p>
                 <div className="child2">
                     <div className="competitions">Wind Turbine</div>
                     <div className="competitions">Wind Farm</div>
                     <div className="competitions">Project Outreach</div>
                 </div>
+                <div className="video-row">
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/FtkoJrZdJhY"
+                        title="Prepare for Tomorrow's Wind Energy Career Today with the Collegiate Wind Competition (CWC)"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+
+                    <iframe
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/C_iIaec6Dtg?si=PubxdslwRm2mTBym"
+                        title="BLADES OF GLORY: An Energy.gov Mini-Doc"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+
             </div>
 
         </div>
