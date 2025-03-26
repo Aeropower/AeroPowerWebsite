3c7258d1648f614a0dd56d686e348ae1a7a0b961
ernestosoto3 <142848535+ernestosoto3@users.noreply.github.com>
2025-03-25 20:41:01 -0400

    Create sponsorpage.jsx

---
+++ src/components/pages/sponsors/sponsorpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../layout/header';
+import Footer from "../../layout/footer";
+
+const SponsorPage = () => {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+        
+    )
+}
+
+export default SponsorPage;
