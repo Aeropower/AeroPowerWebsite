6c28664ad335221efc005d7f4f0b1dfcad4dda67
ernestosoto3 <ernesto.soto3@upr.edu>
2025-04-06 20:27:23 -0400

    Improved Sponsor Page

--- src/components/pages/sponsors/sponsorpage.jsx
+++ src/components/pages/sponsors/sponsorpage.jsx
@@ -6,16 +6,17 @@ import SponsorshipTiers from './SponsorshipTiers';
 import SponsorFooter from './SponsorFooter';
 
 const SponsorPage = () => {
-    return (
-        <div className="dark:bg-gray-900 dark:text-white transition-colors duration-300">
-            <Header />
-            <Info />
-            <Sponsors />
-            <SponsorshipTiers />
-            <SponsorFooter />
-        </div>
-
-    )
-}
+  return (
+    <div className="dark:bg-gray-900 dark:text-white transition-colors duration-500 ease-in-out font-sans">
+      <Header />
+      <main>
+        <Info />
+        <Sponsors />
+        <SponsorshipTiers />
+      </main>
+      <SponsorFooter />
+    </div>
+  );
+};
 
 export default SponsorPage;
